4e94e7364e3cef667826d157dc5339b01c4c41c5
leandrohsilveira <leandro.hinckel@gmail.com>
2017-04-05 12:35:52 -0300

    Aula 04

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,23 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { MenuTestPage } from "../pages/menu-test/menu-test";
 
+interface Page {
+  component: any,
+  title: string,
+  icon: string
+}
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
+  pages: Page[] = [
+    {component: HomePage, title: 'Home', icon: 'home'},
+    {component: MenuTestPage, title: 'Menu Test', icon: 'user'},
+  ];
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -18,6 +28,10 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
-    });
+    }); 
+  }
+
+  openPage(page: Page): void {
+    this.rootPage = page.component;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { MenuTestPage } from '../pages/menu-test/menu-test';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -9,7 +10,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    MenuTestPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -17,7 +19,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    MenuTestPage
   ],
   providers: [
     StatusBar,
